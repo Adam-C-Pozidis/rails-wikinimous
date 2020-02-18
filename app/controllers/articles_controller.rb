1501a47da0c999ec0a05a0b2e0d2ff40f3ff4c2c
Adam Pozidis <adchrpoz@gmail.com>
2020-02-18 23:08:50 +0100

    wiki

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,28 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+    @new_article = Article.new
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    article = Article.find(params[:id])
+    article.update(params_article)
+    redirect_to article_path(article)
+  end
+
+  def destroy
+    article = Article.find(params[:id])
+    article.delete
+    redirect_to articles_path
+  end
+
+ private
+
+  def params_article
+    params["article"].permit(:title, :content)
+  end
+end
